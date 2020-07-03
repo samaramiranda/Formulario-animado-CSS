3304603ab1fb56d4266de5ad0686dee8c268e0ed
samaramiranda <samaramiranda.13@hotmail.com>
2020-07-03 19:09:10 -0300

    Animação de submit do formulário

--- desafios/03/script.js
+++ desafios/03/script.js
@@ -2,7 +2,7 @@ const btnLogin = document.querySelector(".btn-login")
 const form = document.querySelector("form")
 
 
-btnLogin.addEventListener("click", (event) => { //função callback para o evento
+btnLogin.addEventListener("click", (event) => { //função do evento
   event.preventDefault(); //removendo comportamento padrão
   form.classList.add("form-hide")//adicionando classe para o form sumir quando clicar
 })
